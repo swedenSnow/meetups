0138e262c2a90fcd75088fe5c75d2ee920c1c857
haz <abuzeid2222@gmail.com>
2020-01-06 13:13:07 +0100

    :syringe: injecting new comp into grids

--- src/Meetups/MeetupGrid.svelte
+++ src/Meetups/MeetupGrid.svelte
@@ -1,26 +1,44 @@
 <script>
     import MeetupItem from './MeetupItem.svelte';
+    import MeetupFilter from './MeetupFilter.svelte';
 
     export let meetups;
+    let favsOnly = false;
+
+    function setFilter(event) {
+        favsOnly = event.detail === 1;
+    }
+
+    //! Reactive statement
+    $: filteredMeetups = favsOnly
+        ? meetups.filter(meet => meet.isFavorite)
+        : meetups;
 </script>
 
 <style>
-    section {
+    #meetups {
         width: 100%;
         display: grid;
         grid-template-columns: 1fr;
         grid-gap: 1rem;
     }
 
+    #meetup-controls {
+        margin: 1rem;
+    }
+
     @media (min-width: 768px) {
-        section {
+        #meetups {
             grid-template-columns: repeat(2, 1fr);
         }
     }
 </style>
 
+<section id="meetup-controls">
+    <MeetupFilter on:select={setFilter} />
+</section>
 <section id="meetups">
-    {#each meetups as meetup}
+    {#each filteredMeetups as meetup}
         <MeetupItem
             id={meetup.id}
             title={meetup.title}
